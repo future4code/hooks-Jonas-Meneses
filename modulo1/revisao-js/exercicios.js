d04392f58674571b1425e7628eb9e30318a94680
JonasLevy <jonaslevy0408@gmail.com>
2022-03-18 20:20:04 -0300

    revisao-js

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -33,32 +33,86 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    return array.filter(item =>{
+        return item%2 === 0
+    }).map(item =>{ return item*item})
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let maiorNumero = array[0]
+  for(let i of array){
+      if(i>maiorNumero){
+          maiorNumero = i
+      }
+  }
+  return maiorNumero
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    
+    let idMaiorNumero = (n1,n2) =>{
+        let maiorNumero
+        let menorNumero
+        if(num1 > num2){ 
+            maiorNumero = n1
+            menorNumero = n2
+        }else{
+            maiorNumero = n2
+            menorNumero = n1
+        }
+        return {maior: maiorNumero, menor: menorNumero} 
+    }
+    let objectNumbers = idMaiorNumero(num1, num2)
+    let higherNumber = objectNumbers.maior
+    let smallestNumber = objectNumbers.menor
+    let maiorDivisivelPorMenorVar = higherNumber % smallestNumber === 0
+    let diferencaVar = higherNumber - smallestNumber
 
+    return { 
+        maiorNumero:higherNumber, maiorDivisivelPorMenor: maiorDivisivelPorMenorVar, diferenca:diferencaVar}
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let number = 0
+   let newArray = []
+    for(let i = 0; i<n; i++){
+        newArray.push(number)
+        number += 2
+   }
+
+   return newArray
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    // Se um triângulo possuir os três lados iguais, ele é chamado de "Equilátero". 
+    // Se possuir apenas dois lados iguais, diz-se que ele é "Isósceles"
+    // Se os três lados tiverem medidas diferentes, ele é "Escaleno".
+    let triangulo = ""
+    if(ladoA === ladoB && ladoB ===ladoC){
+        triangulo = "Equilátero"
+    }else if(ladoA === ladoB || ladoB ===ladoC || ladoA === ladoC){
+        triangulo = "Isósceles"
+    }else{
+        triangulo = "Escaleno"
+    }
+    return triangulo
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    let newArray =  array.sort(function(a, b) {
+        return a - b;
+      })
+    
+    let arrayFinal = []
+    arrayFinal.push(newArray[0])
+    arrayFinal.push(newArray[1])
+    
+    return arrayFinal
 }
 
 // EXERCÍCIO 11
