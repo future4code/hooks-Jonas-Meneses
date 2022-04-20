4305ebd29380f9a28c8a36eca0a235c298ed881e
JonasLevy <jonaslevy0408@gmail.com>
2022-04-19 23:11:35 -0300

    Desafio

---
+++ Modulo-3/jsx-labetube/desafio/youtube/src/App.js
@@ -0,0 +1,244 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <header>
+          <div className='logo-seach'>
+            <div className='logo'>
+                <img src=''/>
+                <p>YouTube</p>
+            </div>
+            <div className='seach'>
+              <input type={'search'} placeholder="Foi o q deu pra fazer o cansaço bateu"/>
+            </div>
+          </div>
+          <div className='preferencias'>
+            <div className='preferencisCard'>
+                <p> dicas de video</p>
+            </div>
+            <div className='preferencisCard'>
+                <p> dicas de video</p>
+            </div>
+            <div className='preferencisCard'>
+                <p> dicas de video</p>
+            </div>
+            <div className='preferencisCard'>
+                <p> dicas de video</p>
+            </div>
+            <div className='preferencisCard'>
+                <p> dicas de video</p>
+            </div>
+          </div>
+      </header>
+      <section className='barraLateral'>
+        <div className='menuBarras'>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div className='menus'>
+           <p> Home</p>
+        </div>
+        <div className='menus'>
+           <p> Explorar</p>
+        </div>
+        <div className='menus'>
+            <p> Shorts</p>
+        </div>
+        <div className='menus'>
+            <p> Inscrições</p>
+        </div>
+        <div className='menus'>
+           <p> Biblioteca</p>
+        </div>
+      </section>
+
+      <main className='gridVideos'>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        <div className='cardVideos'>
+          <div className='imgVideo'>
+
+          </div>
+          <div className='imgcanal-Titulo'>
+            <div className='img-canal'>
+
+            </div>
+            <div className='tituloVideo'>
+              <h2> titudo do video - um texto grande para ver se vai sair da caixa</h2>
+            </div>
+          </div>
+          <div className='nomeCanal-Inscritos'>
+            <div className='nomecanal'>
+              <p>Nome do canal</p>
+            </div>
+            <div className='Visualizacao'>
+              <p> 26 mil</p>
+            </div>
+          </div>
+        </div>
+
+        
+
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
