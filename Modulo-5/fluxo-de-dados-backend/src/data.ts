0d4ae216a108263f983776126bd0b6cbefdad345
Jonas Levy <jonaslevy0408@gmail.com>
2022-08-23 23:44:20 -0300

    exercicio fluxo de dados

---
+++ Modulo-5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,26 @@
+type Produto = {
+    id: string,
+    name: string,
+    price: number
+
+}
+
+export let produtos: Produto[] = [
+    {
+        id: "aaa",
+        name: "leite",
+        price: 5
+    },
+
+    {
+        id: "aba",
+        name: "chocolate",
+        price: 4.5
+    },
+
+    {
+        id: "abb",
+        name: "Mucilon",
+        price: 9
+    }
+]
