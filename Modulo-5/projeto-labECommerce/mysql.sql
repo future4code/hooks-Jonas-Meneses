5cda65d8d5bd212ffa7bd9a6c60ed3127be97abd
Jonas Levy <jonaslevy0408@gmail.com>
2022-09-20 19:58:35 -0300

    criei o sql

---
+++ Modulo-5/projeto-labECommerce/mysql.sql
@@ -0,0 +1,9 @@
+-- Active: 1663294692132@@35.226.146.116@3306@hooks-4313394-jonas-meneses
+
+
+CREATE TABLE Labecommerce_users(
+    id VARCHAR(100) PRIMARY KEY NOT NULL,
+    name VARCHAR(150) NOT NULL,
+    
+
+)
