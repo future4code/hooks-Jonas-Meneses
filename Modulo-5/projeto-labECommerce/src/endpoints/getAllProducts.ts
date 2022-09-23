3fc3c1591e52f2365118b698267c6bdcca4d07cc
Jonas Levy <jonaslevy0408@gmail.com>
2022-09-23 00:35:09 -0300

    fiz  a 1 do desafio

--- Modulo-5/projeto-labECommerce/src/endpoints/getAllProducts.ts
+++ Modulo-5/projeto-labECommerce/src/endpoints/getAllProducts.ts
@@ -4,8 +4,27 @@ import connection from "../dataBase/connection";
 const getAllProducts = async (req: Request, res: Response) => {
 
     try {
+        let order = req.query.order as string
+        let search = req.query.search as string
+
+        let orderBy = ""
+        
+         if(order && order.toUpperCase() === "ASC"){
+
+            orderBy = `ORDER BY name ${order.toUpperCase()}`
+         }
+
+         if(order && order.toUpperCase() === "DESC"){
+
+            orderBy = `ORDER BY name ${order.toUpperCase()}`
+         }
+
+         if(!search) search = "%"
+
+
         const result = await connection.raw(`
-            SELECT * FROM labecommerce_products;
+            SELECT * FROM labecommerce_products
+            WHERE name LIKE "%${search}%" ${orderBy}
         `)
 
         if (await result[0].length < 1) {
