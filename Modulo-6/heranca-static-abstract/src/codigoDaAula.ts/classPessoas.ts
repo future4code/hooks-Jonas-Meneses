cf4dae0bc39ed55d11c87fd1a2d6868de97f987e
Jonas Levy <jonaslevy0408@gmail.com>
2022-09-29 23:01:38 -0300

    Exercicio

---
+++ Modulo-6/heranca-static-abstract/src/codigoDaAula.ts/classPessoas.ts
@@ -0,0 +1,11 @@
+export class Pessoas {
+
+    constructor(
+        private id: string,
+        private name: string,
+        private email: string,
+        private dataNasc: Date,
+        private turma: string,
+
+    ){}
+}
